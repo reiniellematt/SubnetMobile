d1c9855baa158ef09269efd242b7172deeda0fdb
reiniellematt <erecereinielle722@gmail.com>
2018-07-26 20:51:26 +0800

    Android manifest modified for the app name

---
+++ SubnetMobile/SubnetMobile/Models/IpAddress.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SubnetMobile.Models
+{
+    /// <summary>
+    /// A class that represents an IP Address.
+    /// </summary>
+    public class IpAddress
+    {
+        /// <summary>
+        /// The first octet or the IP Address.
+        /// </summary>
+        public short FirstOctet { get; set; }
+
+        /// <summary>
+        /// The second octet of the IP Address.
+        /// </summary>
+        public short SecondOctet { get; set; }
+
+        /// <summary>
+        /// The third octet of the IP Address.
+        /// </summary>
+        public short ThirdOctet { get; set; }
+
+        /// <summary>
+        /// The fourth octet of the IP Address.
+        /// </summary>
+        public short FourthOctet { get; set; }
+
+        public override string ToString()
+        {
+            return $"{ FirstOctet }.{ SecondOctet }.{ ThirdOctet }.{ FourthOctet }";
+        }
+    }
+}
